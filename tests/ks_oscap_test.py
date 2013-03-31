85c0c011b5d3e5d215e7a3e063aba1cfd923c9c8
Vratislav Podzimek <vpodzime@redhat.com>
2013-03-31 13:11:25 +0200

    Add test for the OSCAPdata's __str__ method
    Also check if the output is correctly parsed by another instance.

--- tests/ks_oscap_test.py
+++ tests/ks_oscap_test.py
@@ -6,8 +6,6 @@ from org_fedora_oscap.ks.oscap import OSCAPdata
 class ParsingTest(unittest.TestCase):
     def setUp(self):
         self.oscap_data = OSCAPdata("org_fedora_oscap")
-
-    def parsing_test(self):
         for line in ["content-type = datastream\n",
                      "content-url = https://example.com/hardening.xml\n",
                      "datastream-id = id_datastream_1\n",
@@ -16,9 +14,30 @@ class ParsingTest(unittest.TestCase):
                      ]:
             self.oscap_data.handle_line(line)
 
+    def parsing_test(self):
         self.assertEqual(self.oscap_data.content_type, "datastream")
         self.assertEqual(self.oscap_data.content_url,
                          "https://example.com/hardening.xml")
         self.assertEqual(self.oscap_data.datastream_id, "id_datastream_1")
         self.assertEqual(self.oscap_data.xccdf_id, "id_xccdf_new")
         self.assertEqual(self.oscap_data.profile_id, "Web Server")
+
+    def str_test(self):
+        str_ret = str(self.oscap_data)
+        self.assertEqual(str_ret,
+                         "%addon org_fedora_oscap\n"
+                         "    content-type = datastream\n"
+                         "    content-url = https://example.com/hardening.xml\n"
+                         "    datastream-id = id_datastream_1\n"
+                         "    xccdf-id = id_xccdf_new\n"
+                         "    profile = Web Server\n"
+                         "%end"
+                         )
+    def str_parse_test(self):
+        self.oscap_data2 = OSCAPdata("org_fedora_oscap")
+        str_ret = str(self.oscap_data)
+        for line in str_ret.splitlines()[1:-1]:
+            self.oscap_data2.handle_line(line)
+
+        str_ret2 = str(self.oscap_data)
+        self.assertEqual(str_ret, str_ret2)
