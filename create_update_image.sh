14633a9a9540df7506ec2c89e50997f57bf7e3d2
Matěj Týč <matyc@redhat.com>
2018-06-20 10:53:04 +0200

    Added releasever option to the image creation script.

--- create_update_image.sh
+++ create_update_image.sh
@@ -2,9 +2,13 @@
 
 build_dir=$PWD
 
-# ARG_POSITIONAL_SINGLE([start-with],[Action to start with],[install_rpms])
+actions=(download_rpms install_rpms install_addon_from_repo create_image cleanup)
+
+
+# ARG_POSITIONAL_SINGLE([start-with],[Action to start with - one of: ${actions[*]}],[install_rpms])
 # ARG_OPTIONAL_SINGLE([rpm-dir],[],[Where to put/take from RPMs to install],[$build_dir/rpm])
 # ARG_OPTIONAL_SINGLE([tmp-root],[],[Fake temp root],[$(mktemp -d)])
+# ARG_OPTIONAL_SINGLE([releasever],[r],[Version of the target OS],[28])
 # ARG_TYPE_GROUP_SET([action],[ACTION],[start-with],[download_rpms,install_rpms,install_addon_from_repo,create_image,cleanup],[index])
 # ARG_HELP([])
 # ARGBASH_GO()
@@ -25,7 +29,7 @@ die()
 begins_with_short_option()
 {
 	local first_option all_short_options
-	all_short_options='h'
+	all_short_options='rh'
 	first_option="${1:0:1}"
 	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
 }
@@ -49,15 +53,17 @@ action()
 _positionals=()
 _arg_start_with="install_rpms"
 # THE DEFAULTS INITIALIZATION - OPTIONALS
-_arg_tmp_root="$(mktemp -d)"
 _arg_rpm_dir="$build_dir/rpm"
+_arg_tmp_root="$(mktemp -d)"
+_arg_releasever="28"
 
 print_help ()
 {
-	printf 'Usage: %s [--tmp-root <arg>] [--rpm-dir <arg>] [-h|--help] [<start-with>]\n' "$0"
-	printf '\t%s\n' "<start-with>: Action to start with (default: 'install_rpms')"
-	printf '\t%s\n' "--tmp-root: Fake temp root (default: '$(mktemp -d)')"
+	printf 'Usage: %s [--rpm-dir <arg>] [--tmp-root <arg>] [-r|--releasever <arg>] [-h|--help] [<start-with>]\n' "$0"
+	printf '\t%s\n' "<start-with>: Action to start with - one of: ${actions[*]} (default: 'install_rpms')"
 	printf '\t%s\n' "--rpm-dir: Where to put/take from RPMs to install (default: '$build_dir/rpm')"
+	printf '\t%s\n' "--tmp-root: Fake temp root (default: '$(mktemp -d)')"
+	printf '\t%s\n' "-r,--releasever: Version of the target OS (default: '28')"
 	printf '\t%s\n' "-h,--help: Prints help"
 }
 
@@ -67,6 +73,14 @@ parse_commandline ()
 	do
 		_key="$1"
 		case "$_key" in
+			--rpm-dir)
+				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
+				_arg_rpm_dir="$2"
+				shift
+				;;
+			--rpm-dir=*)
+				_arg_rpm_dir="${_key##--rpm-dir=}"
+				;;
 			--tmp-root)
 				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
 				_arg_tmp_root="$2"
@@ -75,13 +89,16 @@ parse_commandline ()
 			--tmp-root=*)
 				_arg_tmp_root="${_key##--tmp-root=}"
 				;;
-			--rpm-dir)
+			-r|--releasever)
 				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
-				_arg_rpm_dir="$2"
+				_arg_releasever="$2"
 				shift
 				;;
-			--rpm-dir=*)
-				_arg_rpm_dir="${_key##--rpm-dir=}"
+			--releasever=*)
+				_arg_releasever="${_key##--releasever=}"
+				;;
+			-r*)
+				_arg_releasever="${_key##-r}"
 				;;
 			-h|--help)
 				print_help
@@ -141,12 +158,9 @@ packages="
 "
 
 
-actions=(download_rpms install_rpms install_addon_from_repo create_image cleanup)
-
-
 download_rpms() {
 	mkdir -p "$rpmdir"
-	(cd "$rpmdir" && dnf download --arch x86_64,noarch $packages)
+	(cd "$rpmdir" && dnf download --arch x86_64,noarch --releasever "$_arg_release" $packages)
 }
 
 
@@ -180,7 +194,7 @@ cleanup() {
 
 sudo true || die "Unable to get sudo working, bailing out."
 
-for (( action_index=_arg_start_with_index;  action_index < ${#actions[*]}; action_index++ )) do 
+for (( action_index=_arg_start_with_index;  action_index < ${#actions[*]}; action_index++ )) do
 	"${actions[$action_index]}"
 done
 
