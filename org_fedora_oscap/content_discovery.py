b1047222be606859acc380a4652a3970e01e3eaf
Jan Černý <jcerny@redhat.com>
2023-03-03 11:32:44 +0100

    Stop accessing self._addon_data
    Stop accessing self._addon_data in the _analyze_fetched_content
    to reduce dependency on PolicyData

--- org_fedora_oscap/content_discovery.py
+++ org_fedora_oscap/content_discovery.py
@@ -162,7 +162,12 @@ class ContentBringer:
             self._finish_actual_fetch(fetching_thread_name)
             if fingerprint and dest_filename:
                 self._verify_fingerprint(fingerprint)
-            content = self._analyze_fetched_content(fetching_thread_name, fingerprint, dest_filename)
+            expected_path = self._addon_data.preinst_content_path
+            expected_tailoring = self._addon_data.preinst_tailoring_path
+            expected_cpe_path = self._addon_data.cpe_path
+            content = self._analyze_fetched_content(
+                fetching_thread_name, fingerprint, dest_filename,
+                expected_path, expected_tailoring, expected_cpe_path)
         except Exception as exc:
             what_if_fail(exc)
             content = None
@@ -232,7 +237,9 @@ class ContentBringer:
             reduced_files[path] = label
         return reduced_files
 
-    def _analyze_fetched_content(self, wait_for, fingerprint, dest_filename):
+    def _analyze_fetched_content(
+                self, wait_for, fingerprint, dest_filename, expected_path,
+                expected_tailoring, expected_cpe_path):
         actually_fetched_content = wait_for is not None
         fpaths = self._gather_available_files(actually_fetched_content, dest_filename)
 
@@ -242,9 +249,6 @@ class ContentBringer:
             structured_content.add_content_archive(dest_filename)
 
         labelled_filenames = content_handling.identify_files(fpaths)
-        expected_path = self._addon_data.preinst_content_path
-        expected_tailoring = self._addon_data.preinst_tailoring_path
-        expected_cpe_path = self._addon_data.cpe_path
         labelled_filenames = self.filter_discovered_content(
             labelled_filenames, expected_path, expected_tailoring,
             expected_cpe_path)
