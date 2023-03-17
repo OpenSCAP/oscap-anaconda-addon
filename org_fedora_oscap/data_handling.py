31b0ade1a5b91456608871870f4c48d72089ec87
Jan Černý <jcerny@redhat.com>
2023-03-17 14:36:29 +0100

    Move methods from OSCAPSpoce to DataHandler
    These 2 methods mainly modify the PolicyData, therefore the
    DataHandler is a better place for them than the GUI.

--- org_fedora_oscap/data_handling.py
+++ org_fedora_oscap/data_handling.py
@@ -1,3 +1,6 @@
+from org_fedora_oscap import common
+
+
 class DataHandler:
     def __init__(self, policy_data) -> None:
         self._policy_data = policy_data
@@ -7,3 +10,31 @@ class DataHandler:
             self._policy_data.content_url and
             self._policy_data.content_type != "scap-security-guide"
         )
+
+    def use_system_content(self):
+        self._policy_data.clear_all()
+        self._policy_data.content_type = "scap-security-guide"
+        self._policy_data.content_path = common.get_ssg_path()
+
+    def use_downloaded_content(self, content):
+        preferred_content = content.get_preferred_content(
+            self._policy_data.content_path)
+
+        # We know that we have ended up with a datastream-like content,
+        # but if we can't convert an archive to a datastream.
+        # self._policy_data.content_type = "datastream"
+        content_type = self._policy_data.content_type
+        if content_type in ("archive", "rpm"):
+            self._policy_data.content_path = str(
+                preferred_content.relative_to(content.root))
+        else:
+            self._policy_data.content_path = str(preferred_content)
+
+        preferred_tailoring = content.get_preferred_tailoring(
+            self._policy_data.tailoring_path)
+        if content.tailoring:
+            if content_type in ("archive", "rpm"):
+                self._policy_data.tailoring_path = str(
+                    preferred_tailoring.relative_to(content.root))
+            else:
+                self._policy_data.tailoring_path = str(preferred_tailoring)
--- org_fedora_oscap/gui/spokes/oscap.py
+++ org_fedora_oscap/gui/spokes/oscap.py
@@ -458,7 +458,7 @@ class OSCAPSpoke(NormalSpoke):
 
         try:
             if actually_fetched_content:
-                self._use_downloaded_content(content)
+                self.data_handler.use_downloaded_content(content)
 
             preinst_content_path = common.get_preinst_content_path(self._policy_data)
             preinst_tailoring_path = common.get_preinst_tailoring_path(self._policy_data)
@@ -1221,34 +1221,6 @@ class OSCAPSpoke(NormalSpoke):
         self._refresh_ui()
 
     def on_use_ssg_clicked(self, *args):
-        self._use_system_content()
+        self.data_handler.use_system_content()
         self._save_policy_data()
         self._fetch_data_and_initialize()
-
-    def _use_system_content(self):
-        self._policy_data.clear_all()
-        self._policy_data.content_type = "scap-security-guide"
-        self._policy_data.content_path = common.get_ssg_path()
-
-    def _use_downloaded_content(self, content):
-        preferred_content = content.get_preferred_content(
-            self._policy_data.content_path)
-
-        # We know that we have ended up with a datastream-like content,
-        # but if we can't convert an archive to a datastream.
-        # self._policy_data.content_type = "datastream"
-        content_type = self._policy_data.content_type
-        if content_type in ("archive", "rpm"):
-            self._policy_data.content_path = str(
-                preferred_content.relative_to(content.root))
-        else:
-            self._policy_data.content_path = str(preferred_content)
-
-        preferred_tailoring = content.get_preferred_tailoring(
-            self._policy_data.tailoring_path)
-        if content.tailoring:
-            if content_type in ("archive", "rpm"):
-                self._policy_data.tailoring_path = str(
-                    preferred_tailoring.relative_to(content.root))
-            else:
-                self._policy_data.tailoring_path = str(preferred_tailoring)
